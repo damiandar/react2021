80a2ca751b0a8eb7e590488bb40b9ac278991f71
drosso <drosso@mpf.gov.ar>
2021-10-29 13:11:23 -0300

    Combo

---
+++ src/components/Combo.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function Combo() {
+  const [valor,setValor]=useState(0);
+  const [items] = useState([
+    {
+      label: "Luke Skywalker",
+      value: "Luke Skywalker"
+    },
+    { label: "C-3PO", value: "C-3PO" },
+    { label: "R2-D2", value: "R2-D2" }
+  ]);
+  return (
+    <div className="App">
+        <h1>Combo</h1>
+        <select onChange={e => setValor(e.currentTarget.value)}>
+        {
+            items.map(item => 
+                (<option key={item.value} value={item.value}>
+                    {item.label}
+                </option>)
+            )
+        }
+        </select>
+        Valor seleccionado: {valor}
+    </div>
+  );
+}
